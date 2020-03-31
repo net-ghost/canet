470f5e721bcbe94a1c0d1e3020b87e21bbaabcb5
netghost <60497242+net-ghost@users.noreply.github.com>
2020-03-31 18:10:38 -0500

    Add files via upload

---
+++ js/m-o.js
@@ -0,0 +1,26 @@
+
+function temporada1(){
+	document.getElementById('temp-1').style.display="block";
+	document.getElementById('temp-2').style.display="none";
+	document.getElementById('temp-3').style.display="none";
+	document.getElementById('temp-4').style.display="none";
+}
+function temporada2(){
+	document.getElementById('temp-1').style.display="none";
+	document.getElementById('temp-2').style.display="block";
+	document.getElementById('temp-3').style.display="none";
+	document.getElementById('temp-4').style.display="none";
+	document.getElementById('temp-4').style.display="none";
+}
+function temporada3(){
+	document.getElementById('temp-1').style.display="none";
+	document.getElementById('temp-2').style.display="none";
+	document.getElementById('temp-3').style.display="block";
+	document.getElementById('temp-4').style.display="none";
+}
+function temporada4(){
+	document.getElementById('temp-1').style.display="none";
+	document.getElementById('temp-2').style.display="none";
+	document.getElementById('temp-3').style.display="none";
+	document.getElementById('temp-4').style.display="block";
+}
